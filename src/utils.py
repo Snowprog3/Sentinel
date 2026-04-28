52095564d6b120c5bcca39dfffc3f1122184bf51
Tom <snowprog3@example.com>
2026-04-28 18:01:07 +0300

    feat: add cler_function and tests

---
+++ src/utils.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from urllib.parse import urlparse
+
+
+def build_filename_from_url(url: str, output_dir: str = "data/raw") -> Path:
+    """Create the path for HTML file`s saving on URL-address"""
+    parsed = urlparse(url)
+    path_part = parsed.path.strip("/")
+    if not path_part:
+        filename = "index.html"
+    else:
+        filename = Path(path_part).name or "index.html"
+    return Path(output_dir) /filename
+
+def ensure_dir(path: Path) -> None:
+    path.parent.mkdir(parents=True, exist_ok=True)
