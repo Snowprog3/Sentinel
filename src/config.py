6f42aa04b4c67a7a20a0e68c17ad1c4f1e0802fe
Tom <snowprog3@example.com>
2026-05-07 12:58:53 +0300

    fix: add new library in pyproject.toml

--- src/config.py
+++ src/config.py
@@ -1,15 +1,4 @@
-import os
-
 from pydantic_settings import BaseSettings
-
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = int(os.getenv("DB_PORT", "5432"))
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
-TIMEOUT = float(os.getenv("REQUEST_TIMEOUT", "10.0"))
-DEBUG = os.getenv("DEBUG", "false").lower() == "true"
 
 
 class Settings(BaseSettings):
--- src/database.py
+++ src/database.py
@@ -1,44 +1,8 @@
 """Database connection utilites for PostgreSQL"""
 
-import os
-from pathlib import Path
-
-import asyncpg
-from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from config import settings
-
-
-async def get_pool() -> asyncpg.Pool:
-    """Create and return pool"""
-    env_path = Path(__file__).parent.parent / ".env"
-    load_dotenv(env_path)
-
-    host = os.getenv("DB_HOST", "localhost")
-    port = os.getenv("DB_PORT", "5432")
-    user = os.getenv("DB_USER")
-    password = os.getenv("DB_PASSWORD")
-    database = os.getenv("DB_NAME", "postgres")
-
-    # Валидация
-    if not user:
-        raise ValueError("DB_USER must be set in .env")
-    if not password:
-        raise ValueError("DB_PASSWORD must be set in .env")
-
-    # Создаём пул соединений
-    pool = await asyncpg.create_pool(
-        host=host,
-        port=port,
-        user=user,
-        password=password,
-        database=database,
-        min_size=1,  # минимум 1 соединение в пуле
-        max_size=10,  # максимум 10 соединений
-    )
-    return pool
-
 
 engine = create_async_engine(settings.DATABASE_URL, echo=True)
 AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
